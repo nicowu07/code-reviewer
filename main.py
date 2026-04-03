1d387949bd194be5dea0f98757e42623b280de75
Nico Wu <ver0n1ca_wu@yahoo.com>
2026-04-03 04:15:56 +0000

    add error handling

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.exceptions import RequestValidationError
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+# prevent information disclosure
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    return templates.TemplateResponse(
+        request=request,
+        name='error.html'
+    )
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return templates.TemplateResponse(
+        request=Request,
+        name='error.html'
+    )
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
@@ -14,9 +30,10 @@ async def home(request: Request):
     )
 
 @app.post("/review", response_class=HTMLResponse)
-async def review(request: Request, code: str = Form(...)):
+async def review(request: Request, code: str = Form("")):
     line_count = len(code.splitlines())
     char_count = len(code)
+    # code limit check
     if char_count > 50000:
         result = "Code limit reached, Please split in several submits!"
         return templates.TemplateResponse(
@@ -24,6 +41,15 @@ async def review(request: Request, code: str = Form(...)):
             name='index.html',
             context={"result": result, "code": ""}
         )
+    # empty code check
+    elif char_count == 0:
+        result = "No code submitted."
+        return templates.TemplateResponse(
+            request=request,
+            name='index.html',
+            context={"result":result, "code": ""}
+        )
+    # normal response
     result = f"Received {line_count} lines of code. Analysis coming soon!"
     return templates.TemplateResponse(
         request=request,
