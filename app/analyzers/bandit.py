5f581ee54f3ea8fc58a4eb7ec9dc8d06b844a0d3
Nico Wu <ver0n1ca_wu@yahoo.com>
2026-04-09 10:55:57 +0000

    NEW: add result parsing for bandit

--- app/analyzers/bandit.py
+++ app/analyzers/bandit.py
@@ -1,11 +1,36 @@
 import subprocess
 import os
+import json
 
 def bandit_analyzer(filePath):
     result = subprocess.run(
-        ["bandit", "-r", filePath],
+        ["bandit", "-r", filePath, "-f", "json"],
         capture_output=True,
         text=True
     )
     os.remove(filePath)
-    return result.stdout
+    if result.returncode == 2:
+        return f"Error running Bandit: {result.stderr}"
+    issue_num, issues, returnCode = parse_bandit_output(result.stdout)
+    if returnCode:
+        return "Parsing result failed"
+    else:
+        return "Analyse finished, get results (link)"
+
+def parse_bandit_output(jsonOutput):
+    try:
+        data = json.loads(jsonOutput)
+    except:
+        return [], [], 1
+    totals = data["metrics"].get("_totals", {})
+    issue_num = {k: v for k, v in totals.items() if k.startswith("SEVERITY") or k.startswith("CONFIDENCE")}
+    issues = []
+    for i in data["results"]:
+        detail = {}
+        detail["code"] = i["code"]
+        detail["issue_confidence"] = i["issue_confidence"]
+        detail["issue_severity"] = i["issue_severity"]
+        detail["issue_text"] = i["issue_text"]
+        issues.append(detail)
+    returnCode = 0
+    return issue_num, issues, returnCode
