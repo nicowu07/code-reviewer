523ca4551f67f345db6e15060aed4463856a5eb3
Nico Wu <ver0n1ca_wu@yahoo.com>
2026-04-25 13:11:31 +0000

    NEW: Get prepared for LLM

--- app/config.py
+++ app/config.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 from fastapi.templating import Jinja2Templates
+import os
 
 BASE_DIR = Path(__file__).parent.parent
 TEMPLATES_DIR = BASE_DIR / "templates"
 templates = Jinja2Templates(directory=str(TEMPLATES_DIR))
 LOGS_DIR = BASE_DIR / "logs"
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
